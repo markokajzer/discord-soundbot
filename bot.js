593b21749d716adb21acaafca52b6fc11be95af1
markokajzer <markokajzer91@gmail.com>
2016-07-10 13:29:44 +0200

    Initial commit

---
+++ bot.js
@@ -0,0 +1,72 @@
+var fs = require('fs');
+var Discord = require('discord.js');
+
+console.log('Use the following URL to let the bot join your server!');
+console.log('https://discordapp.com/oauth2/authorize?client_id=201081661658628097&scope=bot');
+
+var bot = new Discord.Client();
+
+bot.on('message', (message) => {
+  // Only listen for messages starting with '!'
+  if(!message.content.startsWith('!')) {
+    return;
+  }
+
+  // Get stored sounds
+  let sounds = fs.readdirSync('sounds/');
+  sounds = sounds.filter((sound) => {
+    return sound.indexOf('.mp3') >= 0;
+  });
+  sounds = sounds.map((sound) => {
+    return sound.split('.')[0];
+  });
+
+  // List available sounds
+  if(message.content === '!sounds') {
+    listAvailableSounds(sounds, message.channel.id);
+    return;
+  }
+
+  // Abort if user is not connected to any voice channel
+  let voiceChannel = message.author.voiceChannel;
+  if(voiceChannel === null) {
+    return;
+  }
+
+  // Abort if users request was on another channel
+  if(voiceChannel.server.id !== message.channel.server.id) {
+    return;
+  }
+
+  // If file sound exists, play it
+  let sound = message.content.split('!')[1];
+  if(sounds.includes(sound)) {
+    playSound(voiceChannel, sound);
+  }
+});
+
+function listAvailableSounds(sounds, channel) {
+  let message = sounds.map((sound) => {
+    return sound;
+  });
+  bot.sendMessage(channel, message);
+}
+
+function playSound(voiceChannel, sound) {
+  let file = `sounds/${sound}.mp3`;
+  bot.joinVoiceChannel(voiceChannel, (error, connection) => {
+    if(error) {
+      console.log('Error occurred!');
+      console.log(error);
+    }
+    else {
+      connection.playFile(file, (_, intent) => {
+        intent.on('end', () => {
+          bot.leaveVoiceChannel(connection);
+        });
+      });
+    }
+  });
+}
+
+bot.loginWithToken('MjAxMDgxNzU3NjQ5MzM4MzY4.CmJ2yQ.9J9EVru6wpWSrVZZ3SToktwg3lY');
