3ee6bd1e7d7fa8cb11dce7a6e10cf9fb0eb5fd20
markokajzer <markokajzer91@gmail.com>
2016-11-07 03:32:31 +0900

    Updated to discordjs 10
    Fixed some stuff according to eslint

--- README.md
+++ README.md
@@ -8,10 +8,7 @@ A Bot for Discord to play your favorite sounds or music.
 ### Requirements
 
 + Due to compatibility with ES2015, **Node.js v6.0.0** or newer is required.
-+ Refering to the [discord.js library](http://discordjs.readthedocs.io/en/latest/troubleshooting.html), three more requirements are needed to use voice functionality:
-  + **FFmpeg**
-  + **Python 2.7.x**
-  + C++ compiler tool for your platform: **build-essential** (Linux), **Xcode Command Line Tools** (Mac OS)
++ For voice functionality you need **FFmpeg**.
 
 
 ### Installation guide
--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-const config  = require('config');
-const fs      = require('fs');
+const config = require('config');
+const fs = require('fs');
 const Discord = require('discord.js');
 
 console.log('Use the following URL to let the bot join your server!');
@@ -8,82 +8,85 @@ console.log(`https://discordapp.com/oauth2/authorize?client_id=${config.get('cli
 const bot = new Discord.Client();
 let queue = [];
 
+bot.login(config.get('token'));
+
 bot.on('message', (message) => {
   // Abort when PM
-  if(message.channel instanceof Discord.DMChannel) {
+  if (message.channel instanceof Discord.DMChannel) {
     return;
   }
 
   // Only listen for messages starting with '!'
-  if(!message.content.startsWith('!')) {
+  if (!message.content.startsWith('!')) {
     return;
   }
 
   // Show list of commands
-  if(message.content === '!commands') {
+  if (message.content === '!commands') {
     listCommands(message.author);
     return;
   }
 
   // Get stored sounds
   let sounds = fs.readdirSync('sounds/');
-  sounds = sounds.filter((sound) => sound.includes('.mp3'));
-  sounds = sounds.map((sound) => sound.split('.')[0]);
+  sounds = sounds.filter(sound => sound.includes('.mp3'));
+  sounds = sounds.map(sound => sound.split('.')[0]);
 
   // Show list of available sounds
-  if(message.content === '!sounds') {
+  if (message.content === '!sounds') {
     listAvailableSounds(sounds, message.author);
     return;
   }
 
   // Remove specified sound
-  if(message.content.startsWith('!remove ')) {
-    let sound = message.content.replace('!remove ', '');
-    if(sounds.includes(sound)) {
+  if (message.content.startsWith('!remove ')) {
+    const sound = message.content.replace('!remove ', '');
+    if (sounds.includes(sound)) {
       removeSound(sound);
       message.channel.sendMessage(`${sound} removed!`);
-    }
-    else {
+    } else {
       message.channel.sendMessage(`${sound} not found!`);
     }
     return;
   }
 
-  let voiceChannel = message.member.voiceChannel;
+  const voiceChannel = message.member.voiceChannel;
 
   // Abort if user is not connected to any voice channel
-  if(voiceChannel === null) {
+  if (voiceChannel === null) {
+    message.reply('Join a voice channel first!');
     return;
   }
 
   // Abort if users request was on another server
-  if(voiceChannel.server.id !== message.channel.server.id) {
-    return;
-  }
+  // TODO CHECK FOR THIS BUG
+  // if(voiceChannel.server.id !== message.channel.server.id) {
+  //  return;
+  // }
 
   // Stop playing and clear queue
-  if(message.content === '!stop') {
+  if (message.content === '!stop') {
     voiceChannel.leave();
     queue = [];
     return;
   }
 
   // Play random sound
-  if(message.content === '!random') {
-    let random = sounds[Math.floor(Math.random() * sounds.length)];
+  if (message.content === '!random') {
+    const random = sounds[Math.floor(Math.random() * sounds.length)];
     playSound(voiceChannel, random);
     return;
   }
 
   // Play it if sound specified and exists
-  let sound = message.content.split('!')[1];
-  if(sounds.includes(sound)) {
+  const sound = message.content.split('!')[1];
+  if (sounds.includes(sound)) {
     playSound(voiceChannel, sound);
   }
 });
 
 function listCommands(user) {
-  let message = [
+  const message = [
     '```',
     '!commands         Show this message',
     '!sounds           Show available sounds',
@@ -97,49 +100,41 @@ function listCommands(user) {
 }
 
 function listAvailableSounds(sounds, user) {
-  let message = sounds.map((sound) => sound);
+  const message = sounds.map(sound => sound);
   user.sendMessage(message);
 }
 
 function removeSound(sound) {
-  let file = `sounds/${sound}.mp3`;
+  const file = `sounds/${sound}.mp3`;
   fs.unlink(file);
 }
 
 function playNext(connection) {
   // Play one sound of queue
-  let file = queue.shift();
-  connection.playFile(file, (_, intent) => {
-    intent.on('end', () => {
-      if(queue.length > 0) {
-        playNext(connection);
-      }
-      else {
-        bot.leaveVoiceChannel(connection);
-      }
-    });
+  const file = queue.shift();
+  const dispatcher = connection.playFile(file);
+  dispatcher.on('end', () => {
+    if (queue.length > 0) {
+      playNext(connection);
+    } else {
+      connection.disconnect();
+    }
   });
 }
 
 function playSound(voiceChannel, sound) {
-  let file = `sounds/${sound}.mp3`;
+  const file = `sounds/${sound}.mp3`;
   queue.push(file);
-  bot.joinVoiceChannel(voiceChannel, (error, connection) => {
-    if(error) {
-      console.log('Error occurred!');
-      console.log(error);
-      bot.leaveVoiceChannel(connection);
+  voiceChannel.join().then((connection) => {
+    // Disallow interruption of sounds, add to queue only instead
+    if (connection.speaking) {
+      return;
     }
-    else {
-      // Disallow interruption of sounds, add to queue instead
-      if(connection.playing) {
-        return;
-      }
 
-      // Work through queue
-      playNext(connection);
-    }
+    // Work through queue
+    playNext(connection);
+  }).catch((error) => {
+    console.log('Error occured!');
+    console.log(error);
   });
 }
-
-bot.login(config.get('token'));
