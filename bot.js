c9363cdb8f196365f439c72062501914face0d9e
markokajzer <markokajzer91@gmail.com>
2016-11-07 20:43:38 +0900

    Use lowdb to store number of times each sound has been played

--- bot.js
+++ bot.js
@@ -1,9 +1,11 @@
 const config = require('config');
 const fs = require('fs');
+const low = require('lowdb');
+const fileAsync = require('lowdb/lib/file-async');
 const Discord = require('discord.js');
 
-console.log('Use the following URL to let the bot join your server!');
-console.log(`https://discordapp.com/oauth2/authorize?client_id=${config.get('client_id')}&scope=bot`);
+const db = low('db.json', { storage: fileAsync });
+db.defaults({ counts: [] }).value();
 
 const bot = new Discord.Client();
 let queue = [];
@@ -20,6 +22,12 @@ bot.on('message', (message) => {
   // Show list of commands
   if (message.content === '!commands') {
     listCommands(message.author);
+    return;
+  }
+
+  // Show number of times the sounds have been played
+  if (message.content === '!mostplayed') {
+    listMostPlayed(message.channel.id);
     return;
   }
 
@@ -68,11 +76,12 @@ bot.on('message', (message) => {
     return;
   }
 
-  // Play it if sound specified and exists
+  // Add sound to queue if exists
   const sound = message.content.split('!')[1];
   if (sounds.includes(sound))
     addToQueue(voiceChannel, sound);
 
+  // Work through queue
   if (bot.voiceConnections.array().length === 0)
     playSoundQueue();
 });
@@ -89,6 +98,29 @@ function listCommands(user) {
     '```'
   ];
   user.sendMessage(message.join('\n'));
+}
+
+function listMostPlayed(channelId) {
+  const sounds = db.get('counts').sortBy('count').reverse().take(15).value();
+  const message = ['```'];
+
+  const longestSound = findLongestWord(sounds.map(sound => sound.name));
+  const longestCount = findLongestWord(sounds.map(sound => String(sound.count)));
+
+  sounds.forEach((sound) => {
+    const spacesForSound = ' '.repeat(longestSound.length - sound.name.length + 1);
+    const spacesForCount = ' '.repeat(longestCount.length - String(sound.count).length);
+    message.push(`${sound.name}:${spacesForSound}${spacesForCount}${sound.count}`);
+  });
+  message.push('```');
+  bot.channels.get(channelId).sendMessage(message.join('\n'));
+}
+
+function findLongestWord(array) {
+  let indexOfLongestWord = 0;
+  for (let i = 1; i < array.length; i++)
+    if (array[indexOfLongestWord].length < array[i].length) indexOfLongestWord = i;
+  return array[indexOfLongestWord];
 }
 
 function listAvailableSounds(sounds, user) {
@@ -109,10 +141,12 @@ function playSoundQueue() {
   const nextSound = queue.shift();
   const file = `sounds/${nextSound.name}.mp3`;
   const voiceChannel = bot.channels.get(nextSound.channel);
+
   voiceChannel.join().then((connection) => {
-    // Work through queue
     const dispatcher = connection.playFile(file);
     dispatcher.on('end', () => {
+      updateCount(nextSound.name);
+
       if (queue.length > 0)
         playSoundQueue();
       else
@@ -124,4 +158,18 @@ function playSoundQueue() {
   });
 }
 
+function updateCount(playedSound) {
+  const sound = db.get('counts').find({ name: playedSound }).value();
+  if (sound) {
+    db.get('counts').find({ name: playedSound }).value().count =
+      db.get('counts').find({ name: playedSound }).value().count + 1;
+    db.write();
+  } else {
+    db.get('counts').push({ name: playedSound, count: 1 }).value();
+  }
+}
+
 bot.login(config.get('token'));
+
+console.log('Use the following URL to let the bot join your server!');
+console.log(`https://discordapp.com/oauth2/authorize?client_id=${config.get('client_id')}&scope=bot`);
