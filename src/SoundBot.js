10e982aa0dbf931b88bbb28a76d706ab63a0bb57
markokajzer <markokajzer91@gmail.com>
2018-03-08 01:37:45 +0100

    Renamed setGame to better reflect what it is doing
    Shuffle code around

--- src/SoundBot.js
+++ src/SoundBot.js
@@ -23,17 +23,17 @@ class SoundBot extends Discord.Client {
   }
 
   _readyListener() {
+    this._setActivity();
     this._setAvatar();
-    this._setGame();
+  }
+
+  _setActivity() {
+    this.user.setActivity(config.get('game'));
   }
 
   _setAvatar() {
     const avatar = Util.avatarExists() ? './config/avatar.png' : null;
     this.user.setAvatar(avatar);
-  }
-
-  _setGame() {
-    this.user.setActivity(config.get('game'));
   }
 
   _messageListener(message) {
