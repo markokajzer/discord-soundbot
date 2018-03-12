851fc62b0a5621e832870d7dc638054d16776ef9
markokajzer <markokajzer91@gmail.com>
2018-03-12 15:31:08 +0100

    Extracted first command, !commands

--- src/Util.ts
+++ src/Util.ts
@@ -29,28 +29,6 @@ class Util {
     return `sounds/${sound}.${this.getExtensionForSound(sound)}`;
   }
 
-  public getListOfCommands() {
-    return [
-      '```',
-      `Use the prefix "${config.prefix}" with the following commands:`,
-      '',
-      'commands             Show this message',
-      'sounds               Show available sounds',
-      'mostplayed           Show 15 most used sounds',
-      'lastadded            Show 5 last added sounds',
-      '<sound>              Play the specified sound',
-      'random               Play random sound',
-      'stop                 Stop playing and clear queue',
-      'leave                Leave the channel',
-      'add                  Add the attached sound',
-      'rename <old> <new>   Rename specified sound',
-      'remove <sound>       Remove specified sound',
-      'ignore <user>        Ignore specified user',
-      'unignore <user>      Unignore specified user',
-      '```'
-    ].join('\n');
-  }
-
   public getMostPlayedSounds() {
     const sounds = this.db.getMostPlayedSounds();
 
---
+++ src/commands/CommandListCommand.ts
@@ -0,0 +1,31 @@
+import { prefix } from '../../config/config.json';
+
+import BaseCommand from './BaseCommand';
+
+export class CommandListCommand extends BaseCommand {
+  public run() {
+    this.message.author.send(this.getListOfCommands());
+  }
+
+  private getListOfCommands() {
+    return [
+      '```',
+      `Use the prefix "${prefix}" with the following commands:`,
+      '',
+      'commands             Show this message',
+      'sounds               Show available sounds',
+      'mostplayed           Show 15 most used sounds',
+      'lastadded            Show 5 last added sounds',
+      '<sound>              Play the specified sound',
+      'random               Play random sound',
+      'stop                 Stop playing and clear queue',
+      'leave                Leave the channel',
+      'add                  Add the attached sound',
+      'rename <old> <new>   Rename specified sound',
+      'remove <sound>       Remove specified sound',
+      'ignore <user>        Ignore specified user',
+      'unignore <user>      Unignore specified user',
+      '```'
+    ].join('\n');
+  }
+}
