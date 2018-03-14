fc8f1be5f7b77ebe7fb44f8316c2f8ac49edcf98
markokajzer <markokajzer91@gmail.com>
2018-03-15 00:46:16 +0100

    Reworked AddCommand

--- src/commands/soundbot/AddCommand.ts
+++ src/commands/soundbot/AddCommand.ts
@@ -1,5 +1,3 @@
-import config from '../../../config/config.json';
-
 import fs from 'fs';
 import https from 'https';
 
@@ -7,51 +5,28 @@ import { MessageAttachment } from 'discord.js';
 
 import BaseCommand from '../base/BaseCommand';
 
-import SoundUtil from '../../util/SoundUtil';
+import AttachmentValidator from '../helpers/AttachmentValidator';
 
 export class AddCommand extends BaseCommand {
   public run() {
-    let error: string | undefined;
-    this.message.attachments.forEach(attachment => {
-      error = this.sanitizeAttachment(attachment);
-    });
+    this.message.attachments.forEach(attachment =>
+      this.saveValidAttachment(attachment));
+  }
 
+  private saveValidAttachment(attachment: MessageAttachment, validator = new AttachmentValidator()) {
+    const error = validator.validAttachment(attachment);
     if (error) {
       this.message.channel.send(error);
       return;
     }
 
-    this.message.attachments.forEach(attachment => {
-      this.addSound(attachment).then(result => {
-        this.message.channel.send(result);
-      });
-    });
-  }
-
-  // @REVIEW Split into submethods.
-  private sanitizeAttachment(attachment: MessageAttachment) {
-    if (attachment.filesize > config.maximumFileSize) {
-      return `${attachment.filename.split('.')[0]} is too big!`;
-    }
-
-    const fileName = attachment.filename.toLowerCase();
-    if (!config.acceptedExtensions.some(ext => fileName.endsWith(ext))) {
-      const extensions = config.acceptedExtensions.join(', ');
-      return `Sound has to be in accepted format, one of [${extensions}]!`;
-    }
-
-    const soundName = fileName.substring(0, fileName.lastIndexOf('.'));
-    if (soundName.match(/[^a-z0-9]/)) {
-      return 'Filename has to be all letters and numbers!';
-    }
-
-    if (SoundUtil.soundExists(soundName)) {
-      return `${soundName} already exists!`;
-    }
+    this.addSound(attachment)
+      .then(result => this.message.channel.send(result))
+      .catch(result => this.message.channel.send(result));
   }
 
   private addSound(attachment: MessageAttachment) {
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
       const fileName = attachment.filename.toLowerCase();
       const soundName = fileName.split('.')[0];
       https.get(attachment.url, response => {
@@ -60,7 +35,7 @@ export class AddCommand extends BaseCommand {
           response.pipe(file);
           resolve(`${soundName} added!`);
         }
-      }).on('error', () => resolve('Something went wrong!'));
+      }).on('error', () => reject('Something went wrong!'));
     });
   }
 }
