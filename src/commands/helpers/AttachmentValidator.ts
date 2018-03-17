094ffbe69219e1d4f9539f3569d35a06e886c938
markokajzer <markokajzer91@gmail.com>
2018-03-17 22:21:55 +0100

    Use private field instead of calling config over and over

--- src/commands/helpers/AttachmentValidator.ts
+++ src/commands/helpers/AttachmentValidator.ts
@@ -5,6 +5,12 @@ import { MessageAttachment } from 'discord.js';
 import SoundUtil from '../../util/SoundUtil';
 
 export default class AttachmentValidator {
+  private acceptedExtensions: Array<string>;
+
+  constructor(acceptedExtensions = config.acceptedExtensions) {
+    this.acceptedExtensions = acceptedExtensions;
+  }
+
   public validAttachment(attachment: MessageAttachment) {
     const fileName = attachment.filename.toLowerCase();
     const soundName = fileName.substring(0, fileName.lastIndexOf('.'));
@@ -17,8 +23,8 @@ export default class AttachmentValidator {
   }
 
   private hasValidExtension(fileName: string) {
-    if (!config.acceptedExtensions.some(ext => fileName.endsWith(ext))) {
-      const extensions = config.acceptedExtensions.join(', ');
+    if (!this.acceptedExtensions.some(ext => fileName.endsWith(ext))) {
+      const extensions = this.acceptedExtensions.join(', ');
       return `Sound has to be in accepted format, one of [${extensions}]!`;
     }
   }
