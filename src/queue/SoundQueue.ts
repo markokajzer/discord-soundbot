ed6a337b0260ed847f05bd70d82fc7f9018eb62e
markokajzer <markokajzer91@gmail.com>
2018-03-18 19:31:00 +0100

    Refactored #playNext

--- src/queue/SoundQueue.ts
+++ src/queue/SoundQueue.ts
@@ -1,5 +1,7 @@
 import config from '../../config/config.json';
 
+import { VoiceConnection } from 'discord.js';
+
 import DatabaseAdapter from '../db/DatabaseAdapter';
 import QueueItem from './QueueItem';
 import SoundUtil from '../util/SoundUtil';
@@ -33,29 +35,26 @@ export default class SoundQueue {
   }
 
   private playNext() {
-    this.current = this.shift();
-    const file = SoundUtil.getPathForSound(this.current.sound);
-    const voiceChannel = this.current.channel;
+    this.current = this.queue.shift()!;
+    const sound = SoundUtil.getPathForSound(this.current.sound);
 
-    voiceChannel.join().then(connection => {
-      connection.playFile(file).on('end', () => {
-        this.db.updateSoundCount(this.current!.sound);
-        if (config.deleteMessages) this.current!.message.delete();
-
-        this.current = null;
-        if (!this.isEmpty()) this.playNext();
-        if (this.isEmpty() && !this.current && !config.stayInChannel) connection.disconnect();
-      });
+    this.current.channel.join().then(connection => {
+      connection.playFile(sound).on('end', () => this.onFinishedPlayingSound(connection));
     }).catch(error => {
       console.error('Error occured!', '\n', error);
     });
   }
 
-  private shift() {
-    return this.queue.shift()!;
-  }
-
   private isEmpty() {
     return this.queue.length === 0;
+  }
+
+  private onFinishedPlayingSound(connection: VoiceConnection) {
+    this.db.updateSoundCount(this.current!.sound);
+    if (config.deleteMessages) this.current!.message.delete();
+
+    this.current = null;
+    if (!this.isEmpty()) this.playNext();
+    if (this.isEmpty() && !this.current && !config.stayInChannel) connection.disconnect();
   }
 }
