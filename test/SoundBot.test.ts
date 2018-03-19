6e62b399a13faefaeb733ca76ae3ba2ae53ceed5
markokajzer <markokajzer91@gmail.com>
2018-03-19 23:01:21 +0100

    Fixed tests for SoundBot

--- test/SoundBot.test.ts
+++ test/SoundBot.test.ts
@@ -1,17 +1,11 @@
-import fs from 'fs';
-import Discord from 'discord.js';
-
 import { token, game } from '../config/config.json'
 import SoundBot from  '../src/SoundBot';
-import MessageHandler from '../src/MessageHandler';
 
-xdescribe('SoundBot', () => {
-  let bot: SoundBot;
-
-  beforeEach(() => {});
+describe('SoundBot', () => {
+  const bot = new SoundBot();
 
   describe('#login', () => {
-    beforeEach(() => {
+    beforeAll(() => {
       spyOn(bot, 'login');
     });
 
@@ -22,16 +16,14 @@ xdescribe('SoundBot', () => {
   });
 
   describe('#addEventListeners', () => {
-    beforeEach(() => {
+    beforeAll(() => {
       spyOn((bot as any), 'setActivity');
-      spyOn((bot as any), 'setAvatar');
       (bot as any).messageHandler = jasmine.createSpyObj('MessageHandler', ['handle']);
     });
 
     it('registers ready listeners', () => {
       bot.emit('ready');
       expect((bot as any).setActivity).toHaveBeenCalledTimes(1);
-      expect((bot as any).setAvatar).toHaveBeenCalledTimes(1);
     });
 
     it('registers message listeners', () => {
@@ -48,36 +40,6 @@ xdescribe('SoundBot', () => {
     it('sets activity', () => {
       (bot as any).setActivity();
       expect(bot.user.setActivity).toHaveBeenCalledWith(game);
-    });
-  });
-
-  describe('#setAvatar', () => {
-    describe('when avatar exists', () => {
-      beforeEach(() => {
-        spyOn(fs, 'existsSync').and.returnValue(true);
-        bot.user = jasmine.createSpyObj('user', ['setAvatar']);
-      });
-
-      it('sets the avatar', () => {
-        expect((bot as any).avatarExists()).toBe(true);
-
-        (bot as any).setAvatar();
-        expect(bot.user.setAvatar).toHaveBeenCalledWith('./config/avatar.png');
-      });
-    });
-
-    describe('when avatar does not exist', () => {
-      beforeEach(() => {
-        spyOn(fs, 'existsSync').and.returnValue(false);
-        bot.user = jasmine.createSpyObj('user', ['setAvatar']);
-      });
-
-      it('sets the avatar', () => {
-        expect((bot as any).avatarExists()).toBe(false);
-
-        (bot as any).setAvatar();
-        expect(bot.user.setAvatar).toHaveBeenCalledWith('');
-      });
     });
   });
 })
