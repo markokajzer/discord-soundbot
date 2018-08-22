8d5cbd9df148173d691a7c33600e02458aa3a633
markokajzer <markokajzer91@gmail.com>
2018-08-22 15:15:24 +0200

    Made sure called methods exist

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import config from '../config/config.json';
 
 import container from './di/DependencyGraph';
 
-const localeService = container.cradle.localeService;
+import SoundBot from './bot/SoundBot';
+import LocaleService from './i18n/LocaleService';
+
+const localeService = container.cradle.localeService as LocaleService;
 localeService.setLocale(config.language);
 
-const bot = container.cradle.soundBot;
+const bot = container.cradle.soundBot as SoundBot;
 bot.start();
 console.log(localeService.t('url', { clientId: config.clientID }));  // tslint:disable-line no-console
