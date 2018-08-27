d972d61f1b004211e0a4260c50da2f57c1e9eb4a
markokajzer <markokajzer91@gmail.com>
2018-08-27 16:51:07 +0200

    Find available locales dynamically

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,8 +1,13 @@
 import i18n = require('i18n');
+
+import fs from 'fs';
 import path from 'path';
 
+const files = fs.readdirSync('./config/locales');
+
+
 i18n.configure({
-  locales: ['en', 'de', 'hu'],
+  locales: files.map(file => file.split('.')[0]),
   defaultLocale: 'en',
   directory: path.join(__dirname, '..', '..', 'config', 'locales'),
   objectNotation: true
