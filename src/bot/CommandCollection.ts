9b5fc9f2d8148a96ab5b53b8e62b48df2a8e2988
markokajzer <markokajzer91@gmail.com>
2019-02-11 00:06:09 +0100

    Refactored CommandCollection to not inherit from Collection

--- src/bot/CommandCollection.ts
+++ src/bot/CommandCollection.ts
@@ -1,17 +1,19 @@
-import { ClientUser, Collection, Message } from 'discord.js';
+import { ClientUser, Message } from 'discord.js';
 
 import Command from './commands/base/Command';
 import UserCommand from './commands/base/UserCommand';
 import SoundCommand from './commands/SoundCommand';
 
-export default class CommandCollection extends Collection<string, Command> {
+export default class CommandCollection {
+  private readonly triggers: Map<string, Command>;
   private readonly commands: Command[];
   private readonly soundCommand: SoundCommand;
 
   constructor(commands: Command[]) {
-    super();
+    this.triggers = new Map();
     this.commands = commands;
     this.soundCommand = commands.find(command => !command.TRIGGERS.length)! as SoundCommand;
+
     this.registerCommands(commands);
   }
 
@@ -21,9 +23,9 @@ export default class CommandCollection extends Collection<string, Command> {
   }
 
   public execute(command: string, params: string[], message: Message) {
-    if (this.has(command)) {
+    if (this.triggers.has(command)) {
       message.content = message.content.substring(command.length + 1);
-      this.get(command)!.run(message, params);
+      this.triggers.get(command)!.run(message, params);
       return;
     }
 
@@ -35,6 +37,6 @@ export default class CommandCollection extends Collection<string, Command> {
   }
 
   private registerTriggers(command: Command) {
-    command.TRIGGERS.forEach(trigger => this.set(trigger, command));
+    command.TRIGGERS.forEach(trigger => this.triggers.set(trigger, command));
   }
 }
