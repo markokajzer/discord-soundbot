cee3cd306cf39d3cc4df5fd8265d65a938aad487
gruntzy <gruntzy@users.noreply.github.com>
2019-02-20 22:32:34 +0100

    Fixing !help message too long (#43)

--- src/bot/commands/HelpCommand.ts
+++ src/bot/commands/HelpCommand.ts
@@ -1,7 +1,7 @@
 import { Message } from 'discord.js';
 
 import Command from './base/Command';
-
+import MessageChunker from './helpers/MessageChunker';
 import Config from '@config/Config';
 import LocaleService from '@util/i18n/LocaleService';
 
@@ -9,59 +9,66 @@ export default class HelpCommand implements Command {
   public readonly TRIGGERS = ['commands', 'help'];
   private readonly config: Config;
   private readonly localeService: LocaleService;
+  private readonly chunker: MessageChunker;
 
-  constructor(config: Config, localeService: LocaleService) {
+  constructor(config: Config, localeService: LocaleService, chunker : MessageChunker) {
     this.config = config;
     this.localeService = localeService;
+    this.chunker = chunker;
   }
 
-  public run(message: Message) {
-    message.author.send(this.getFormattedListOfCommands());
+  public run(message: Message, params: string[]) {
+    const page = parseInt(params[0]);
+
+    [
+      this.localeService.t('help.headline', { prefix: this.config.prefix }),
+      ...  this.chunker.chunkedMessages(this.getFormattedListOfCommands(), page)
+    ].forEach( (chunk: string) => message.author.send(chunk));
+
   }
 
   private getFormattedListOfCommands() {
     return [
-      '```',
-      this.localeService.t('help.headline', { prefix: this.config.prefix }),
-      '',
-      `welcome                          ${this.localeService.t('help.welcome')}`,
-      `commands                         ${this.localeService.t('help.commands')}`,
-      `help                             ${this.localeService.t('help.commands')}`,
+        // this.localeService.t('help.headline', { prefix: this.config.prefix }),
+        `welcome                          ${this.localeService.t('help.welcome')}`,
+        `commands                         ${this.localeService.t('help.commands')}`,
+        `help                             ${this.localeService.t('help.commands')}`,
 
-      `sounds                           ${this.localeService.t('help.sounds.all')}`,
-      `add                              ${this.localeService.t('help.sounds.add')}`,
-      `add <name> <link>                ${this.localeService.t('help.sounds.add')}`,
-      `add <name> <link> <start>        ${this.localeService.t('help.sounds.add')}`,
-      `add <name> <link> <start> <end>  ${this.localeService.t('help.sounds.add')}`,
-      `<sound>                          ${this.localeService.t('help.sounds.play')}`,
-      `combo <sound> ...                ${this.localeService.t('help.sounds.combo')}`,
-      `random                           ${this.localeService.t('help.sounds.random')}`,
-      `random <tag>                     ${this.localeService.t('help.sounds.random')}`,
-      `rename <old> <new>               ${this.localeService.t('help.sounds.rename')}`,
-      `remove <sound>                   ${this.localeService.t('help.sounds.remove')}`,
-      `download <sound>                 ${this.localeService.t('help.sounds.download')}`,
-      `stop                             ${this.localeService.t('help.sounds.stop')}`,
-      `leave                            ${this.localeService.t('help.sounds.stop')}`,
+        `sounds                           ${this.localeService.t('help.sounds.all')}`,
+        `add                              ${this.localeService.t('help.sounds.add')}`,
+        `add <name> <link>                ${this.localeService.t('help.sounds.add')}`,
+        `add <name> <link> <start>        ${this.localeService.t('help.sounds.add')}`,
+        `add <name> <link> <start> <end>  ${this.localeService.t('help.sounds.add')}`,
+        `<sound>                          ${this.localeService.t('help.sounds.play')}`,
+        `combo <sound> ...                ${this.localeService.t('help.sounds.combo')}`,
+        `random                           ${this.localeService.t('help.sounds.random')}`,
+        `random <tag>                     ${this.localeService.t('help.sounds.random')}`,
+        `rename <old> <new>               ${this.localeService.t('help.sounds.rename')}`,
+        `remove <sound>                   ${this.localeService.t('help.sounds.remove')}`,
+        `download <sound>                 ${this.localeService.t('help.sounds.download')}`,
+        `stop                             ${this.localeService.t('help.sounds.stop')}`,
+        `leave                            ${this.localeService.t('help.sounds.stop')}`,
 
-      `entrance <sound>                 ${this.localeService.t('help.entrance.set')}`,
-      `entrance                         ${this.localeService.t('help.entrance.remove')}`,
+        `entrance <sound>                 ${this.localeService.t('help.entrance.set')}`,
+        `entrance                         ${this.localeService.t('help.entrance.remove')}`,
 
-      `tag <sound> <tag>                ${this.localeService.t('help.tags.add')}`,
-      `tag <sound>                      ${this.localeService.t('help.tags.list')}`,
-      `tag <sound> clear                ${this.localeService.t('help.tags.clear')}`,
-      `tags                             ${this.localeService.t('help.tags.all')}`,
-      `search <tag>                     ${this.localeService.t('help.tags.search')}`,
+        `tag <sound> <tag>                ${this.localeService.t('help.tags.add')}`,
+        `tag <sound>                      ${this.localeService.t('help.tags.list')}`,
+        `tag <sound> clear                ${this.localeService.t('help.tags.clear')}`,
+        `tags                             ${this.localeService.t('help.tags.all')}`,
+        `search <tag>                     ${this.localeService.t('help.tags.search')}`,
 
-      `mostplayed                       ${this.localeService.t('help.mostplayed')}`,
-      `lastadded                        ${this.localeService.t('help.lastadded')}`,
+        `mostplayed                       ${this.localeService.t('help.mostplayed')}`,
+        `lastadded                        ${this.localeService.t('help.lastadded')}`,
 
-      `ignore <user>                    ${this.localeService.t('help.ignore')}`,
-      `unignore <user>                  ${this.localeService.t('help.unignore')}`,
-      `avatar                           ${this.localeService.t('help.avatar')}`,
-      `avatar remove                    ${this.localeService.t('help.avatar')}`,
-      `config <option> <value>          ${this.localeService.t('help.config')}`,
-      `set <option> <value>             ${this.localeService.t('help.config')}`,
-      '```'
-    ].join('\n');
+        `ignore <user>                    ${this.localeService.t('help.ignore')}`,
+        `unignore <user>                  ${this.localeService.t('help.unignore')}`,
+        `avatar                           ${this.localeService.t('help.avatar')}`,
+        `avatar remove                    ${this.localeService.t('help.avatar')}`,
+        `config <option> <value>          ${this.localeService.t('help.config')}`,
+        `set <option> <value>             ${this.localeService.t('help.config')}`,
+
+    ];
+
   }
 }
