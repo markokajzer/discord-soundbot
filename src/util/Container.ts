962b636789f414c0013f2f606f61150eb55022ca
markokajzer <markokajzer91@gmail.com>
2020-01-26 14:34:44 +0100

    Remove awilix and build own dependency graph

--- src/util/Container.ts
+++ src/util/Container.ts
@@ -1,75 +1,100 @@
-import * as awilix from 'awilix';
-
-import path from 'path';
-
 import Config from '@config/Config';
 import SoundQueue from '@queue/SoundQueue';
-import SoundBot from '../bot/SoundBot';
 import CommandCollection from '../bot/CommandCollection';
+import MessageHandler from '../bot/MessageHandler';
+import SoundBot from '../bot/SoundBot';
 
-const container = awilix.createContainer({
-  injectionMode: awilix.InjectionMode.CLASSIC
-});
+import YoutubeDownloader from '../bot/commands/helpers/downloader/YoutubeDownloader';
+import YoutubeValidator from '../bot/commands/helpers/downloader/validator/YoutubeValidator';
+import AttachmentDownloader from '../bot/commands/helpers/downloader/AttachmentDownloader';
+import AttachmentValidator from '../bot/commands/helpers/downloader/validator/AttachmentValidator';
 
-container.register({
-  config: awilix.asClass(Config).singleton(),
-  queue: awilix.asClass(SoundQueue).singleton()
-});
+import AddCommand from '../bot/commands/AddCommand';
+import AvatarCommand from '../bot/commands/AvatarCommand';
+import ComboCommand from '../bot/commands/ComboCommand';
+import ConfigCommand from '../bot/commands/ConfigCommand';
+import DownloadCommand from '../bot/commands/DownloadCommand';
+import EntranceCommand from '../bot/commands/EntranceCommand';
+import ExitCommand from '../bot/commands/ExitCommand';
+import HelpCommand from '../bot/commands/HelpCommand';
+import IgnoreCommand from '../bot/commands/IgnoreCommand';
+import LastAddedCommand from '../bot/commands/LastAddedCommand';
+import LoopCommand from '../bot/commands/LoopCommand';
+import MostPlayedCommand from '../bot/commands/MostPlayedCommand';
+import NextCommand from '../bot/commands/NextCommand';
+import PingCommand from '../bot/commands/PingCommand';
+import RandomCommand from '../bot/commands/RandomCommand';
+import RemoveCommand from '../bot/commands/RemoveCommand';
+import RenameCommand from '../bot/commands/RenameCommand';
+import SearchCommand from '../bot/commands/SearchCommand';
+import SkipCommand from '../bot/commands/SkipCommand';
+import SoundCommand from '../bot/commands/SoundCommand';
+import SoundsCommand from '../bot/commands/SoundsCommand';
+import StopCommand from '../bot/commands/StopCommand';
+import TagCommand from '../bot/commands/TagCommand';
+import TagsCommand from '../bot/commands/TagsCommand';
+import UnignoreCommand from '../bot/commands/UnignoreCommand';
+import WelcomeCommand from '../bot/commands/WelcomeCommand';
 
-container.loadModules(['bot/**/*.js'], {
-  cwd: path.join(__dirname, '..'),
-  formatName: 'camelCase',
-  resolverOptions: {
-    lifetime: awilix.Lifetime.SINGLETON,
-    register: awilix.asClass
-  }
-});
+export const config = new Config();
+const queue = new SoundQueue(config);
 
-container.register({
-  commands: awilix
-    .asClass(CommandCollection)
-    .singleton()
-    .inject(() => ({
-      commands: [
-        container.cradle.pingCommand,
+const attachmentValidator = new AttachmentValidator(config);
+const attachmentDownloader = new AttachmentDownloader(attachmentValidator);
 
-        container.cradle.addCommand,
-        container.cradle.renameCommand,
-        container.cradle.removeCommand,
+const youtubeValidator = new YoutubeValidator();
+const youtubeDownloader = new YoutubeDownloader(youtubeValidator);
 
-        container.cradle.soundCommand,
-        container.cradle.comboCommand,
-        container.cradle.randomCommand,
-        container.cradle.loopCommand,
-        container.cradle.nextCommand,
-        container.cradle.skipCommand,
-        container.cradle.stopCommand,
+const commands = [
+  new PingCommand(),
 
-        container.cradle.entranceCommand,
-        container.cradle.exitCommand,
+  // SOUND PLAYING RELATED COMMANDS
+  new SoundCommand(queue),
+  new ComboCommand(queue),
+  new RandomCommand(queue),
+  new LoopCommand(queue),
+  new NextCommand(queue),
+  new SkipCommand(queue),
+  new StopCommand(queue),
 
-        container.cradle.soundsCommand,
-        container.cradle.searchCommand,
-        container.cradle.tagCommand,
-        container.cradle.tagsCommand,
-        container.cradle.downloadCommand,
+  // ENTRANCE / EXIT SOUNDS
+  new EntranceCommand(),
+  new ExitCommand(),
 
-        container.cradle.welcomeCommand,
-        container.cradle.helpCommand,
-        container.cradle.lastAddedCommand,
-        container.cradle.mostPlayedCommand,
-        container.cradle.ignoreCommand,
-        container.cradle.unignoreCommand,
+  // SOUND ADMINISTRATION COMMANDS
+  new AddCommand(attachmentDownloader, youtubeDownloader),
+  new SoundsCommand(config),
+  new SearchCommand(),
+  new RenameCommand(),
+  new RemoveCommand(),
+  new TagCommand(),
+  new TagsCommand(),
+  new DownloadCommand(),
 
-        container.cradle.avatarCommand,
-        container.cradle.configCommand
-      ]
-    }))
-});
+  // HELP / INFO COMMANDS
+  new WelcomeCommand(config),
+  new HelpCommand(config),
+  new LastAddedCommand(),
+  new MostPlayedCommand(),
+
+  // CONFIGURATION RELATED COMMANDS
+  new AvatarCommand(config),
+  new ConfigCommand(config),
+  new IgnoreCommand(),
+  new UnignoreCommand()
+];
+
+const commandCollection = new CommandCollection(commands);
+const messageHandler = new MessageHandler(config, commandCollection);
+
+const soundBot = new SoundBot(config, commandCollection, messageHandler, queue);
 
 interface SoundBotContainer {
   config: Config;
   soundBot: SoundBot;
 }
 
-export default container.cradle as SoundBotContainer;
+export default {
+  config,
+  soundBot
+} as SoundBotContainer;
