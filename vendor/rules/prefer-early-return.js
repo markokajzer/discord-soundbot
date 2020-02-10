bcdf1cd42be5e60f5b3dc38b3fda9473c041e08c
markokajzer <markokajzer91@gmail.com>
2020-02-10 21:14:09 +0100

    Simplified custom eslint rule

--- vendor/rules/prefer-early-return.js
+++ vendor/rules/prefer-early-return.js
@@ -1,9 +1,10 @@
 // Taken from eslint-plugin-shopify
 // https://github.com/Shopify/eslint-plugin-shopify/blob/c5e0f7919d28fe69e0b56d82fc00d6d96b513783/lib/rules/prefer-early-return.js
 
-const docsUrl = 'https://github.com/Shopify/eslint-plugin-shopify/blob/c5e0f7919d28fe69e0b56d82fc00d6d96b513783/docs/rules/prefer-early-return.md'
+const docsUrl =
+  'https://github.com/Shopify/eslint-plugin-shopify/blob/c5e0f7919d28fe69e0b56d82fc00d6d96b513783/docs/rules/prefer-early-return.md';
 
-const defaultMaximumStatements = 1;
+const MAXIMUM_STATEMENTS = 1;
 
 module.exports = {
   meta: {
@@ -12,48 +13,27 @@ module.exports = {
         'Prefer early returns over full-body conditional wrapping in function declarations.',
       category: 'Best Practices',
       recommended: false,
-      uri: docsUrl,
+      uri: docsUrl
     },
-    schema: [
-      {
-        type: 'object',
-        properties: {
-          maximumStatements: {
-            type: 'integer',
-          },
-        },
-        additionalProperties: false,
-      },
-    ],
+    schema: []
   },
 
   create(context) {
-    const options = context.options[0] || {
-      maximumStatements: defaultMaximumStatements,
-    };
-    const maxStatements = options.maximumStatements;
-
     function isLonelyIfStatement(statement) {
       return statement.type === 'IfStatement' && statement.alternate == null;
     }
 
     function isOffendingConsequent(consequent) {
-      return (
-        (consequent.type === 'ExpressionStatement' && maxStatements === 0) ||
-        (consequent.type === 'BlockStatement' &&
-          consequent.body.length > maxStatements)
-      );
+      return consequent.type === 'BlockStatement' && consequent.body.length > MAXIMUM_STATEMENTS;
     }
 
     function isOffendingIfStatement(statement) {
-      return (
-        isLonelyIfStatement(statement) &&
-        isOffendingConsequent(statement.consequent)
-      );
+      return isLonelyIfStatement(statement) && isOffendingConsequent(statement.consequent);
     }
 
     function hasSimplifiableConditionalBody(functionBody) {
-      const body = functionBody.body;
+      const { body } = functionBody;
+
       return (
         functionBody.type === 'BlockStatement' &&
         body.length === 1 &&
@@ -62,20 +42,17 @@ module.exports = {
     }
 
     function checkFunctionBody(functionNode) {
-      const body = functionNode.body;
+      const { body } = functionNode;
 
       if (hasSimplifiableConditionalBody(body)) {
-        context.report(
-          body,
-          'Prefer an early return to a conditionally-wrapped function body',
-        );
+        context.report(body, 'Prefer an early return to a conditionally-wrapped function body');
       }
     }
 
     return {
       FunctionDeclaration: checkFunctionBody,
       FunctionExpression: checkFunctionBody,
-      ArrowFunctionExpression: checkFunctionBody,
+      ArrowFunctionExpression: checkFunctionBody
     };
-  },
+  }
 };
