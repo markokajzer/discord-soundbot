d1c9054a5216486e6b6586fd738a108b34fb8443
markokajzer <markokajzer91@gmail.com>
2020-02-11 19:40:19 +0100

    chore: remove custom types

--- src/types/replace-in-file.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'replace-in-file' {
-  interface ReplaceOptions {
-    files: string[];
-    from: RegExp[];
-    to: (...args: string[]) => string;
-  }
-
-  function sync(options: ReplaceOptions): string[];
-}
