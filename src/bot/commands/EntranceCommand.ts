0cddbc1b3209dfadffa962a265e085a1228d7abb
zopz <z@zops.me>
2020-05-29 23:33:45 -0500

    add ability to adjust other users entrance

--- src/bot/commands/EntranceCommand.ts
+++ src/bot/commands/EntranceCommand.ts
@@ -1,4 +1,4 @@
-import { Message } from 'discord.js';
+import { Message, Permissions } from 'discord.js';
 
 import * as entrances from '@util/db/Entrances';
 import { getSounds } from '@util/SoundUtil';
@@ -6,17 +6,32 @@ import Command from './base/Command';
 
 export default class EntranceCommand implements Command {
   public readonly TRIGGERS = ['entrance'];
+  public readonly NUMBER_OF_PARAMETERS = 2;
+  public readonly USAGE = 'Usage: !entrance <sound> ?[user_id]';
 
   public run(message: Message, params: string[]) {
-    const [entranceSound] = params;
-    if (!entranceSound) {
-      entrances.remove(message.author.id);
+    if (params.length > this.NUMBER_OF_PARAMETERS) {
+      message.channel.send(this.USAGE);
+      return;
+    }
+
+    if (params.length === 1) {
+      var [entrance] = params;
+      var user_id = message.author.id;
+    }else{
+      if (!message.member) return;
+      
+      var [entrance, user_id] = params;
+    }
+
+    if (!entrance) {
+      entrances.remove(user_id);
       return;
     }
 
     const sounds = getSounds();
-    if (!sounds.includes(entranceSound)) return;
+    if (!sounds.includes(entrance)) return;
 
-    entrances.add(message.author.id, entranceSound);
+    entrances.add(user_id, entrance);
   }
 }
