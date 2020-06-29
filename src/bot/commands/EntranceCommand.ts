084d69967c04eb2f38b709524caa2904e43ef2d8
Zach Pierce <zpierce@reviewtrackers.com>
2020-06-29 17:47:19 -0500

    fix linting

--- src/bot/commands/EntranceCommand.ts
+++ src/bot/commands/EntranceCommand.ts
@@ -7,32 +7,34 @@ import Command from './base/Command';
 export default class EntranceCommand implements Command {
   public readonly TRIGGERS = ['entrance'];
   public readonly NUMBER_OF_PARAMETERS = 2;
-  public readonly USAGE = 'Usage: !entrance <sound> ?<user_id>';
+  public readonly USAGE = 'Usage: !entrance <sound> ?<userId>';
 
   public run(message: Message, params: string[]) {
+    let entranceSound;
+    let userId;
     if (params.length > this.NUMBER_OF_PARAMETERS) {
       message.channel.send(this.USAGE);
       return;
     }
 
     if (params.length === 1) {
-      var [entranceSound] = params;
-      var user_id = message.author.id;
+      [entranceSound] = params;
+      userId = message.author.id;
     } else {
       if (!message.member) return;
       if (!message.member.hasPermission(Permissions.FLAGS.ADMINISTRATOR!)) return;
 
-      var [entranceSound, user_id] = params;
+      [entranceSound, userId] = params;
     }
 
     if (!entranceSound) {
-      entrances.remove(user_id);
+      entrances.remove(userId);
       return;
     }
 
     const sounds = getSounds();
     if (!sounds.includes(entranceSound)) return;
 
-    entrances.add(user_id, entranceSound);
+    entrances.add(userId, entranceSound);
   }
 }
--- src/bot/commands/ExitCommand.ts
+++ src/bot/commands/ExitCommand.ts
@@ -7,32 +7,34 @@ import Command from './base/Command';
 export default class ExitCommand implements Command {
   public readonly TRIGGERS = ['exit'];
   public readonly NUMBER_OF_PARAMETERS = 2;
-  public readonly USAGE = 'Usage: !exit <sound> ?<user_id>';
+  public readonly USAGE = 'Usage: !exit <sound> ?<userId>';
 
   public run(message: Message, params: string[]) {
+    let exitSound;
+    let userId
     if (params.length > this.NUMBER_OF_PARAMETERS) {
       message.channel.send(this.USAGE);
       return;
     }
 
     if (params.length === 1) {
-      var [exitSound] = params;
-      var user_id = message.author.id;
+      [exitSound] = params;
+      userId = message.author.id;
     } else {
       if (!message.member) return;
       if (!message.member.hasPermission(Permissions.FLAGS.ADMINISTRATOR!)) return;
 
-      var [exitSound, user_id] = params;
+      [exitSound, userId] = params;
     }
 
     if (!exitSound) {
-      exits.remove(user_id);
+      exits.remove(userId);
       return;
     }
 
     const sounds = getSounds();
     if (!sounds.includes(exitSound)) return;
 
-    exits.add(user_id, exitSound);
+    exits.add(userId, exitSound);
   }
 }
