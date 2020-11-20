4fa386691df915ca84d33531c24dd22985780564
Marko Kajzer <markokajzer91@gmail.com>
2020-11-21 00:29:08 +0100

    chore(DefaultConfig): format

--- src/config/DefaultConfig.ts
+++ src/config/DefaultConfig.ts
@@ -1,34 +1,21 @@
 import ConfigInterface from './ConfigInterface';
 
 const DEFAULT_CONFIG: ConfigInterface = {
-  acceptedExtensions: ['.mp3', '.wav'],
   clientId: '',
+  token: '',
 
-  // Minutes
-deafen: false,
-  
-deleteMessages: false,
-  
-elevatedRoles: [],
-  
-game: 'SoundBoard', 
-  
-language: 'en',
-  
-
-maximumFileSize: 1000000,
-  
-
-prefix: '!',
-  
-
-stayInChannel: false, 
-  
-timeout: 10,
-  
-token: '',
-  // 1 MB
-volume: 1.0
+  // eslint-disable-next-line sort-keys-fix/sort-keys-fix
+  acceptedExtensions: ['.mp3', '.wav'],
+  deafen: false,
+  deleteMessages: false,
+  elevatedRoles: [],
+  game: 'SoundBoard',
+  language: 'en',
+  maximumFileSize: 1000000, // 1 MB
+  prefix: '!',
+  stayInChannel: false,
+  timeout: 10, // Minutes
+  volume: 1.0
 };
 
 export default DEFAULT_CONFIG;
